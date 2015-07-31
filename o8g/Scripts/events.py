a8edf98fb6f76ea26e45943ae4768c945ea4954b
raohmaru <namoracon@gmail.com>
2015-08-01 00:24:31 +0200

    Only align backups on move cards in the table

--- o8g/Scripts/events.py
+++ o8g/Scripts/events.py
@@ -41,7 +41,7 @@ def onMoveCard(player, card, fromGroup, toGroup, oldIndexs, indexs, oldX, oldY, 
          clearAttachLinks(card)
          freeSlot(card)
    elif fromGroup == table and toGroup == table:
-      if card.Type == 'Character':
+      if card.Type == 'Character' and (oldX != x or oldY != y):
          alignBackups(card, x, y)
          
 
